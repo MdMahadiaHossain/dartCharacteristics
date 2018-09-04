0ce820e9dced1b97e29792ec343ca641fc98035f
MdMahadiaHossain <mahadihossainpvt@gmail.com>
2018-09-04 16:19:46 +0600

    Update dartFirst.dart

--- dartFirst.dart
+++ dartFirst.dart
@@ -1,33 +1,3 @@
-@// dart class fields and getter
-
-class Rectangle {
-  // this are the fields of Rectangle class
-  final num left, top, right, bottom;
-
-  // fields needs to be initiate by constructor
-  Rectangle(this.left, this.top, this.right, this.bottom);
-
-  
-//CONSIDER using => for simple members.
-
-//In addition to using => for function expressions, 
-//Dart also lets you define members with it. That style is a good fit 
-//for simple members that just calculate and return a value.
-
-double get area => (right - left) * (bottom - top);
-  
-// but if people needs to write multiple lines and needs to use cascade avoid it, instead use block 
-  
-double  area(){
-. . . .
-  . . .  
-}
-  
-  
-  
-}// class ends
-
-
 
 
 @// dart json decoder
