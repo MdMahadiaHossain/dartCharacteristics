b996c98abe5ac8422f4656fc687ba7b4320787f8
MdMahadiaHossain <mahadihossainpvt@gmail.com>
2018-09-04 16:03:25 +0600

    Update dartFirst.dart

--- dartFirst.dart
+++ dartFirst.dart
@@ -1,4 +1,4 @@
-// dart class fields and properties
+@// dart class fields and properties
 
 class Rectangle {
   // this are the fields of Rectangle class
@@ -16,3 +16,34 @@ class Rectangle {
   num get bottom => top + height;
   set bottom(num value) => top = value - height;
 }
+
+@// dart json decoder
+
+//Be sure to use double quotes ("),
+// not single quotes ('), inside the JSON string.
+exe:
+[
+    {"score": 40},
+    {"score": 80}
+]
+
+
+// the "array" of json will be converted into "List" in dart
+
+var jsonString = '''
+  [
+    {"score": 40},
+    {"score": 80}
+  ]
+''';
+
+var scores = jsonDecode(jsonString);
+assert(scores is List);
+
+
+// the object of json will be converted into "Map" in dart
+
+var firstScore = scores[0];
+assert(firstScore is Map);
+assert(firstScore['score'] == 40);
+
