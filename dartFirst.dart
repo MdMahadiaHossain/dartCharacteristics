d6483d4f796a2ba2392a3b3257a6d18af98fc35b
MdMahadiaHossain <mahadihossainpvt@gmail.com>
2018-09-04 15:53:42 +0600

    Update dartFirst.dart

--- dartFirst.dart
+++ dartFirst.dart
@@ -1,3 +1,18 @@
-int calculate() {
-  return 6 * 7;
+// dart class fields and properties
+
+class Rectangle {
+  // this are the fields of Rectangle class
+  final num left, top, width, height;
+
+  // fields needs to be initiate by constructor
+  Rectangle(this.left, this.top, this.width, this.height);
+
+  
+
+  // it is the property of Rectangle class
+  // properties have getter and setter
+  num get right => left + width;
+  set right(num value) => left = value - width;
+  num get bottom => top + height;
+  set bottom(num value) => top = value - height;
 }
